deeb831fc83262ffaf2fd7211b134a8b219d517d
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-10-24 02:59:38 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+import com.tomhuang.ui.GameJFrame;
+import com.tomhuang.ui.LoginJFrame;
+import com.tomhuang.ui.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        // Start file of the game
+        new GameJFrame();
+        new LoginJFrame();
+        new RegisterJFrame();
+
+    }
+}
